61ebb63efe0c4e264d5da2bf320139d6b4e706fc
Valentin Vachev <v.vachevstz@gmail.com>
2022-02-01 13:58:25 +0200

    Initial commit

---
+++ src/main/java/com/vachev/taskmanagementbaeldungcourse/TaskManagementBaeldungCourseApplication.java
@@ -0,0 +1,13 @@
+package com.vachev.taskmanagementbaeldungcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementBaeldungCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementBaeldungCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vachev/taskmanagementbaeldungcourse/TaskManagementBaeldungCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vachev.taskmanagementbaeldungcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementBaeldungCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
